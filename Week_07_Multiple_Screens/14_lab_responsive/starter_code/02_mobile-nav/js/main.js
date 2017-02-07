17f5dea0ff7cbb9e1b639f17d0ee9f14e789ccb8
adamucho <acc88r@gmail.com>
2017-02-06 21:59:52 -0800

    Started on mobile responsive startup matchmaker page, covered "em and rem",

---
+++ Week_07_Multiple_Screens/14_lab_responsive/starter_code/02_mobile-nav/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+$("nav ul li:first-child").click(function(){
+	console.log("hamburger menu clicked");
+	// $("ul li a").slideToggle();
+	$("nav").toggleClass("open");
+	
+
+
+});
+
+});
